a7603e378f4323e18dc788ae12fb47139fe7c151
Cihat Işıker <chtt95@gmail.com>
2019-09-08 15:14:16 +0300

    Hesap Makinesi Yapıldı

--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -16,5 +16,41 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi1=0, sayi2=0, sonuc=0;
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+            sonuc = sayi1 + sayi2;
+            label3.Text = Convert.ToString(sonuc);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double sayi1 = 0, sayi2 = 0, sonuc = 0;
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+            sonuc = sayi1 - sayi2;
+            label3.Text = Convert.ToString(sonuc);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double sayi1 = 0, sayi2 = 0, sonuc = 0;
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+            sonuc = sayi1 * sayi2;
+            label3.Text = Convert.ToString(sonuc);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double sayi1 = 0, sayi2 = 0, sonuc = 0;
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+            sonuc = sayi1 / sayi2;
+            label3.Text = Convert.ToString(sonuc);
+        }
     }
 }
